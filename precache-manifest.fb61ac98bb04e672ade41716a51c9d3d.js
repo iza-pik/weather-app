d2fd7ee5989a77101765d90f34a2d077864c0956
Izabela <i.j.pikula@gmail.com>
2019-06-19 18:18:58 +0100

    Updates

---
+++ precache-manifest.fb61ac98bb04e672ade41716a51c9d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "609aa7943596a16d4127",
+    "url": "/weather-app/static/css/main.71a8f707.chunk.css"
+  },
+  {
+    "revision": "609aa7943596a16d4127",
+    "url": "/weather-app/static/js/main.c4ccf632.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "13158f613644951cd623",
+    "url": "/weather-app/static/js/2.fd35e841.chunk.js"
+  },
+  {
+    "revision": "e3782bddfb825fec808bb87789e640b2",
+    "url": "/weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.54d45f515babab69c043686168dc5353.js"
+  "/weather-app/precache-manifest.fb61ac98bb04e672ade41716a51c9d3d.js"
 );
 
 workbox.clientsClaim();
